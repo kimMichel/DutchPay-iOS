0165a7868f3d8aa38235de3b9c6e1005613e6bd1
Michel Kim <kim98.michel@gmail.com>
2023-07-03 12:50:44 -0300

    Pass the result to ResultViewController

--- DutchPay/Controller/CalculateViewController.swift
+++ DutchPay/Controller/CalculateViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class CalculateViewController: UIViewController {
+    
+    var tip = 0.1
+    var numberOfPeople = 2
+    var totalBill = 0.0
+    var billPerPeople = "0.0"
 
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPcButton: UIButton!
@@ -16,12 +21,46 @@ class CalculateViewController: UIViewController {
     @IBOutlet weak var splitNumberLabel: UILabel!
 
     @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
+        
+        zeroPcButton.isSelected = false
+        tenPcButton.isSelected = false
+        twentyPcButton.isSelected = false
+        sender.isSelected = true
+        
+        let buttonTitle = sender.currentTitle!
+        let buttonTitleMinusPercentageSign = String(buttonTitle.dropLast())
+        let buttonTitleAsANumber = Double(buttonTitleMinusPercentageSign)!
+        tip = buttonTitleAsANumber / 100
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        billTextField.endEditing(true)
+        splitNumberLabel.text = String(Int(sender.value))
+        numberOfPeople = Int(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        billTextField.endEditing(true)
+        
+        let bill = billTextField.text!
+        
+        if bill != "" {
+            totalBill = Double(bill)!
+            let result = totalBill * (1 + tip) / Double(numberOfPeople)
+            billPerPeople = String(format: "%.2f", result)
+        }
+        
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.result = billPerPeople
+            destinationVC.tip = Int(tip * 100)
+            destinationVC.split = numberOfPeople
+        }
     }
 }
 
---
+++ DutchPay/Controller/ResultViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ResultViewController.swift
+//  DutchPay
+//
+//  Created by Michel Jaejin Kim on 03/07/23.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingLabel: UILabel!
+    
+    var result = "0.0"
+    var tip = 10
+    var split = 2
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        totalLabel.text = result
+        settingLabel.text = "Split between \(split) people, with \(tip)% tip."
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
